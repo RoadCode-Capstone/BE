c59be3fc54319b2fe86fa360a76431d9a0e1da39
hjp <pk1029@yu.ac.kr>
2025-06-09 02:46:56 +0900

    자동배포 test

--- src/main/java/com/capstone2025/roadcode/controller/TestController.java
+++ src/main/java/com/capstone2025/roadcode/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
     @RequestMapping("/hi")
-    public String hi(){
+    public String hii(){
         return "success";
     }
     @RequestMapping("/run")
