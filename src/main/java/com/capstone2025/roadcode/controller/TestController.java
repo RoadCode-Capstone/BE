251a6a859391ac30fa36d337060b998bf3c05786
hjp <pk1029@yu.ac.kr>
2025-06-15 19:24:05 +0900

    fix: 자동 배포 테스트-deploy.sh 수정

--- src/main/java/com/capstone2025/roadcode/controller/TestController.java
+++ src/main/java/com/capstone2025/roadcode/controller/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-    @RequestMapping("/hii")
-    public String hii(){
-        return "success";
+    @RequestMapping("/hi")
+    public String hi(){
+        return "hi test";
     }
 }
