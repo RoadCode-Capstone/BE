8fdb637ac5e91436cdb23ea8b3e91a71eb6c8bbe
hjp <pk1029@yu.ac.kr>
2025-06-17 18:03:08 +0900

    build: 8080포트 죽이고 다시 배포

--- src/main/java/com/capstone2025/roadcode/controller/TestController.java
+++ src/main/java/com/capstone2025/roadcode/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
     @RequestMapping("/hi")
-    public String hitest(){
-        return "hi test";
+    public String hi(){
+        return "hi";
     }
 }
