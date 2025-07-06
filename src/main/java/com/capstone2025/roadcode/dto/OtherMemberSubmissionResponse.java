16c7674d5dd74ce3a3e8789f7b0438a0d67c7482
hjp <pk1029@yu.ac.kr>
2025-07-07 00:15:25 +0900

    feat: SubmissionService-다른사람 풀이 목록 조회에 language 필드 추가

--- src/main/java/com/capstone2025/roadcode/dto/OtherMemberSubmissionResponse.java
+++ src/main/java/com/capstone2025/roadcode/dto/OtherMemberSubmissionResponse.java
@@ -1,5 +1,6 @@
 package com.capstone2025.roadcode.dto;
 
+import com.capstone2025.roadcode.common.LanguageType;
 import com.capstone2025.roadcode.entity.Submission;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 public class OtherMemberSubmissionResponse {
     private Long submissionId;
+    private LanguageType language;
     private String sourceCode;
     private String nickname;
     private LocalDateTime createdAt;
@@ -19,6 +21,7 @@ public class OtherMemberSubmissionResponse {
     public static OtherMemberSubmissionResponse from(Submission submission){
         return new OtherMemberSubmissionResponse(
                 submission.getId(),
+                submission.getLanguage(),
                 submission.getSourceCode(),
                 submission.getMember().getNickname(),
                 submission.getCreatedAt()
