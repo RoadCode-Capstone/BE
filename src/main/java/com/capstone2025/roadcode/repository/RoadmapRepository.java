4b4851aaf9484cc0fdcd2b8d28d9e7fdfb1d3454
chair00 <pk1029@yu.ac.kr>
2025-08-13 13:28:31 +0900

    fix: RoadmapRepository - :status -> :statusList

--- src/main/java/com/capstone2025/roadcode/repository/RoadmapRepository.java
+++ src/main/java/com/capstone2025/roadcode/repository/RoadmapRepository.java
@@ -17,7 +17,7 @@ public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
     List<Roadmap> findByMember(Member member);
 
     // 사용자가 가지고 있는 로드맵 목록 조회(status 필터링)
-    @Query("SELECT r FROM Roadmap r WHERE r.member = :member AND (:status IS NULL OR r.status IN :statusList)")
+    @Query("SELECT r FROM Roadmap r WHERE r.member = :member AND (:statusList IS NULL OR r.status IN :statusList)")
     List<Roadmap> findRoadmapsByMemberAndStatus(
             @Param("member") Member member, @Param("statusList") List<RoadmapStatus> statusList);
 }
