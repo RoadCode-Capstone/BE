4c02aba204706e0c3f8ac51f3dd6ed271bbcab7c
chair00 <pk1029@yu.ac.kr>
2025-11-11 02:33:24 +0900

    FEAT: 개념 강화 문제/추천 문제를 기존 로드맵에 추가(DTO 추가)

---
+++ src/main/java/com/capstone2025/roadcode/dto/AddConceptProblemRequest.java
@@ -0,0 +1,10 @@
+package com.capstone2025.roadcode.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddConceptProblemRequest {
+    private Long currentProblemId;
+}
