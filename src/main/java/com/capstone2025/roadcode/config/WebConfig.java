076c97c44cd7f15b5d16883dcb9e8eca775699c2
chair00 <pk1029@yu.ac.kr>
2025-11-12 16:56:46 +0900

    FIX: cors 주소 추가(웹 배포 주소)

--- src/main/java/com/capstone2025/roadcode/config/WebConfig.java
+++ src/main/java/com/capstone2025/roadcode/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                        "https://roadcode.vercel.app/",
+                        "https://*-parktjgus-projects.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
